2fb2e48795676891e8c9f55c518ca0ae5096f442
Filipe Serafini <filipe.farah@edu.pucrs.br>
2023-05-10 16:11:54 -0300

    creating initial project on XCode

---
+++ AppCaronas/AppCaronas/AppCaronasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCaronasApp.swift
+//  AppCaronas
+//
+//  Created by Filipe Serafini on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppCaronasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCaronas/AppCaronas/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppCaronas
+//
+//  Created by Filipe Serafini on 10/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
