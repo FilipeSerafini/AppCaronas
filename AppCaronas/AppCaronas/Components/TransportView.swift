9410ea7099431679c0107da326604eedd15f3ad7
g-z-m-s <gustavo.zm.silvano@gmail.com>
2023-05-15 15:25:54 -0300

    adicionando os arquivos do Gustavo no projeto

---
+++ AppCaronas/AppCaronas/Components/TransportView.swift
@@ -0,0 +1,44 @@
+//
+//  TransportView.swift
+//  AppCaronas
+//
+//  Created by Gustavo Zahorcsak Matias Silvano on 13/05/23.
+//
+
+import SwiftUI
+
+struct TransportView: View {
+    @State var selected: [Bool] = [false, false, false, false, false, false]
+    let symbolList: [String] = [
+        "iphone.radiowaves.left.and.right",
+        "car.fill",
+        "bicycle",
+        "bus.fill",
+        "figure.walk"
+    ]
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            ForEach(0..<symbolList.count, id: \.self) { i in
+                ZStack {
+                    Circle()
+                        .foregroundColor(selected[i] ? Color(.lightGray) : .white)
+                        .frame(width: 38)
+                    Button {
+                        selected[i].toggle()
+                    } label: {
+                        Image(systemName: symbolList[i])
+                            .foregroundColor(.black)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+    }
+}
+
+struct TransportView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransportView()
+    }
+}
