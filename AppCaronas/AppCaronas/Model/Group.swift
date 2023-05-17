f95d565244fd466af29254467b2e649b14120253
Filipe Serafini <filipe.farah@edu.pucrs.br>
2023-05-16 23:16:27 -0300

    Criei o arquivo UserCRUD e atualizei a struct do grupo com o que precisa para o cloudkit

--- AppCaronas/AppCaronas/Model/Group.swift
+++ AppCaronas/AppCaronas/Model/Group.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CloudKit
 
 
 enum RideType{
@@ -77,32 +78,85 @@ struct Week{
     
 }
 
-struct Group: Hashable{
-    static func == (lhs: Group, rhs: Group) -> Bool {
-        lhs.id == rhs.id
-    }
+struct Group: Hashable, CloudKitItemProtocol{
     
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
+    var record: CKRecord
+
     var id: String = UUID().uuidString
     var type: RideType
     var initialAdress: String
     var finalAdress: String
-    var leader: Person
+    var admin: String
     var maxMembers: Int
     var members: [String]
     var price: Bool
     var time: String
     var week: Week
-//    var monday: Bool = false
-//    var tuesday: Bool = false
-//    var wednesday: Bool = false
-//    var thursday: Bool = false
-//    var friday: Bool = false
-//    var saturday: Bool = false
-//    var sunday: Bool = false
+    
+    //init para transformar os atributos em records para o banco, e determinar o recordType do grupo
+    init?(
+        type: RideType,
+        initialAdress: String,
+        finalAdress: String,
+        admin: String,
+        maxMembers: Int,
+        members: [String],
+        price: Bool,
+        time: String,
+        week: Week) {
+            
+            let record = CKRecord(recordType: "Group")
+            
+            //MARK: DEFINIR COMO VAO SER PASSADOS OS TIPOS NAO NATURAIS, E VERIFICAR O PRICE(?)
+//            record["rideType"] = type
+            record["initialAdress"] = initialAdress
+            record["finalAdress"] = finalAdress
+            record["admin"] = admin
+            record["maxMembers"] = maxMembers
+            record["members"] = members
+            record["price"] = price //deveria chegar um valor aqui, nao? (nao esquecer de alterar no outro init tambem
+            record["time"] = time
+//            record["week"] = week
+            
+            
+            
+        self.init(record: record)
+    }
+    
+    //init para criar o objeto e atribuir o record a ele
+    init?(record: CKRecord) {
+        
+//        guard let rideType = record["rideType"] as? String else { return nil }
+        guard let initialAdress = record["initialAdress"] as? String else { return nil }
+        guard let finalAdress = record["finalAdress"] as? String else { return nil }
+        guard let admin = record["admin"] as? String else { return nil }
+        guard let maxMembers = record["maxMembers"] as? Int else { return nil }
+        guard let members = record["members"] as? [String] else { return nil }
+        guard let price = record["price"] as? Bool else { return nil }
+        guard let time = record["time"] as? String else { return nil }
+        //        guard let week = record["week"] as? String else { return nil }
+        
+        
+//        self.type =
+        self.initialAdress = initialAdress
+        self.finalAdress = finalAdress
+        self.admin = admin
+        self.maxMembers = maxMembers
+        self.members = members
+        self.price = price
+        self.time = time
+//        self.week =
+        
+        self.record = record
+        
+    }
+    
+    
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
     
 }
 
@@ -110,5 +164,6 @@ var groups = [g1, g2]
 
 var weekTeste = Week(selected: [true, false, true, false, false, false, false])
 
-var g1: Group = Group(type: .car, initialAdress: "Rua João Alfredo 277", finalAdress: "PUCRS", leader: p1, maxMembers: 4, members: [p2.name, p3.name], price: true, time: "12:30", week: weekTeste)
-var g2: Group = Group(type: .car, initialAdress: "PUCRS", finalAdress: "Rua João Alfredo 277", leader: p1, maxMembers: 4, members: [p4.name, p5.name], price: false, time: "17:30", week: weekTeste)
+//MARK: VERIFICAR FORCED UNWRAP AQUI
+var g1: Group = Group(type: .car, initialAdress: "Rua João Alfredo 277", finalAdress: "PUCRS", admin: "userID", maxMembers: 4, members: [p2.name, p3.name], price: true, time: "12:30", week: weekTeste)!
+var g2: Group = Group(type: .car, initialAdress: "PUCRS", finalAdress: "Rua João Alfredo 277", admin: "userID2", maxMembers: 4, members: [p4.name, p5.name], price: false, time: "17:30", week: weekTeste)!
